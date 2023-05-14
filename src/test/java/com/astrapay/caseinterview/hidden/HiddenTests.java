634c4a8018ae56019b85c858488c01f6eddf3644
Arthur Purnama <arthur@purnama.de>
2023-05-14 14:24:41 +0700

    add hidden tests

---
+++ src/test/java/com/astrapay/caseinterview/hidden/HiddenTests.java
@@ -0,0 +1,15 @@
+package com.astrapay.caseinterview.hidden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class HiddenTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
