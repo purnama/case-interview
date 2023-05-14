053c04d1b7d86929baa86e49c1232d9d1c21c4b1
Arthur Purnama <arthur@purnama.de>
2023-05-14 13:53:56 +0700

    initial commit and push

---
+++ src/main/java/com/astrapay/caseinterview/CaseInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.astrapay.caseinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astrapay/caseinterview/CaseInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astrapay.caseinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
